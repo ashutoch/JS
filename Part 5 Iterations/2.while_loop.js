f8c318c330b42477198adb60d3622b1c5c39ffe1
ashu <ashutoshmeher885@gmail.com>
2025-11-19 14:55:28 +0530

    Add while and do-while loop examples to demonstrate iteration in JavaScript

---
+++ Part 5 Iterations/2.while_loop.js
@@ -0,0 +1,19 @@
+let i = 1
+while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    i += 2;
+}
+
+let array = [1,2,3,4,5,6,7,8,9,10]
+let a = 0
+while (a < array.length) {
+    // console.log(`Value is ${array[a]}`);
+    a++
+}
+
+// do-while loop
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 10);
